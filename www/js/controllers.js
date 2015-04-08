d67b76420958c1e5f0a13953ff87d7713de45de8
Maddie Clayton <Maddie@nat-oitwireless-inside-vapornet100-c-620.Princeton.EDU>
2015-04-07 20:22:46 -0400

    changes to design of controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,83 +1,118 @@
 angular.module('starter.controllers', [])
 
-// .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-//   // Form data for the login modal
-//   $scope.loginData = {};
-
-//   // Create the login modal that we will use later
-//   $ionicModal.fromTemplateUrl('templates/login.html', {
-//     scope: $scope
-//   }).then(function(modal) {
-//     $scope.modal = modal;
-//   });
-
-//   // Triggered in the login modal to close it
-//   $scope.closeLogin = function() {
-//     $scope.modal.hide();
-//   };
-
-//   // Open the login modal
-//   $scope.login = function() {
-//     $scope.modal.show();
-//   };
-
-//   // Perform the login action when the user submits the login form
-//   $scope.doLogin = function() {
-//     console.log('Doing login', $scope.loginData);
-
-//     // Simulate a login delay. Remove this and replace with your login
-//     // code if using a login system
-//     $timeout(function() {
-//       $scope.closeLogin();
-//     }, 1000);
-//   };
-// })
-
 
 
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.Forbes = [
-    { title: 'Beef and Rice Noodles', id: 1 },
-    { title: 'Chicken with Mushrooms and Leeks', id: 2 },
-    { title: 'Vegetables and Couscous', id: 3 },
-    { title: 'Hummus', id: 4 }
-  ];
-  $scope.Wu = [
-    { title: 'Chicken Tenders', id: 1 },
-    { title: 'Corn', id: 2 },
-    { title: 'Shoestring Fries', id: 3 },
-    { title: 'Chicken Rice Soup', id: 4 }
-  ];
-  $scope.Popularity = [
-    { title: 'Chicken Tenders', id: 1 },
-    { title: 'Beef and Rice Noodles', id: 2 },
-    { title: 'Moo Sho Pork', id: 3 },
-    { title: 'Pasta Marinara', id: 4 }
-  ];
-  $scope.Rocky = [
-    { title: 'Pasta Marinara', id: 1 },
-    { title: 'Tortellini Primavera', id: 2 },
-    { title: 'Cheese Pizza', id: 3 },
-    { title: 'Grilled Cheese Trio', id: 4 }
-  ];
-  $scope.Whitman = [
-    { title: 'Fried Catfish Shooter', id: 1 },
-    { title: 'Basil Chicken', id: 2 },
-    { title: 'Hot and Sour Soup', id: 3 },
-    { title: 'Tofu Vietnamese', id: 4 }
-  ];
-  $scope.JCL = [
-    { title: 'Lemon Pollack', id: 1 },
-    { title: 'Black Bean Veggie Couscous', id: 2 },
-    { title: 'Baked Sweet Potato', id: 3 },
-    { title: 'Sauteed Green Beans', id: 4 }
-  ];
-  $scope.Grad = [
-    { title: 'Moo Sho Pork', id: 1 },
-    { title: 'Thai Coconut Soup', id: 2 },
-    { title: 'Buffalo Chicken Wings', id: 3 },
-    { title: 'Sesame Noodles', id: 4 }
-  ];
+  var items = ["W: Nuggets", "F: Melon"];
+  var filters = [];
+  var F = false;
+  var Wu = false;
+  var R = false;
+  var Wh = false;
+  var J = false;
+  var G = false;
+  $scope.change = function() {
+    items1 = [];
+    for(filter in filters) {
+      for(item in items) {
+        if(item.substring(0, 1) == filter)
+          items1.push(item)
+      }
+    }
+    items = items1;
+    window.location.reload(true)
+  }
+  $scope.Forbes = function() {
+    if(F) {
+      filters.push("F")
+    }
+    else {
+      filter1 = [];
+      for(filter in filters) {
+        if(filter != "F") {
+          filter1.push(filter);
+        }
+      }
+      filters = filter1;
+      change()
+    }
+  }
+  $scope.Wu = function() {
+    if(F) {
+      filters.push("W")
+    }
+    else {
+      filter1 = [];
+      for(filter in filters) {
+        if(filter != "W") {
+          filter1.push(filter);
+        }
+      }
+      filters = filter1;
+      change()
+    }
+  }
+  $scope.Popularity = items;
+  $scope.Whitman = function() {
+    if(F) {
+      filters.push("H")
+    }
+    else {
+      filter1 = [];
+      for(filter in filters) {
+        if(filter != "H") {
+          filter1.push(filter);
+        }
+      }
+      filters = filter1;
+      change()
+    }
+  }
+  $scope.Rocky = function() {
+    if(F) {
+      filters.push("R")
+    }
+    else {
+      filter1 = [];
+      for(filter in filters) {
+        if(filter != "R") {
+          filter1.push(filter);
+        }
+      }
+      filters = filter1;
+      change()
+    }
+  }
+  $scope.CJL = function() {
+    if(F) {
+      filters.push("C")
+    }
+    else {
+      filter1 = [];
+      for(filter in filters) {
+        if(filter != "C") {
+          filter1.push(filter);
+        }
+      }
+      filters = filter1;
+      change()
+    }
+  }
+  $scope.Grad = function() {
+    if(F) {
+      filters.push("G")
+    }
+    else {
+      filter1 = [];
+      for(filter in filters) {
+        if(filter != "G") {
+          filter1.push(filter);
+        }
+      }
+      filters = filter1;
+      change()
+    }
+  }
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
